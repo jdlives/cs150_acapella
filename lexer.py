1cde900a4ef7feb58737485a01fbc5fc1ca01b95
Treacy <treacyev@gmail.com>
2016-11-30 05:11:32 -0800

    Lexer: Identifying Words

--- lexer.py
+++ lexer.py
@@ -1,11 +1,16 @@
 import sys
+import string
 
 filename = "test.txt"
 f = open("test.txt", 'r')
 
-def lexer(string):
+def lexer(token):
 
-	print(string)
+	checkIdentifier = 0
+	checkNumber = 0
+	checkNotIdentifier = 0
+
+	print(token)
 
 	keywords = "play pause stop repeat chorus duet solo cue final prelude postlude interlude"
 	keyword = keywords.split()
@@ -16,24 +21,58 @@ def lexer(string):
 	twoCharSymbols = "== != >= <= += -= *= /="
 	symbol2 = twoCharSymbols.split()
 
-	identifiers = "Do Re Mi Fa So La Ti lyrics verse album albums symphony symphonies playlist"
-	identifier = identifiers.split()
+	datatypes = "Do Re Mi Fa So La Ti lyrics verse album albums symphony symphonies playlist"
+	datatype = datatypes.split()
+
+	identifiersHead = string.letters
+	identifiersTail = string.letters + string.digits + "_"
+
+	numbersHead = string.digits + "."
+	numbersTail = string.digits + "." + ""
+
+	for i in range(0, len(token)):
+		for j in range(0, len(numbersHead)):
+			if (token[i] == numbersHead[j]):
+				for m in range(0, len(token)):
+					for n in range(0, len(numbersTail)):
+						if (token[m] == numbersTail[n]):
+							checkNumber = 1
+		
+	if (checkNumber == 1):
+		print("NUMBER")
+
 
 	for i in range(0,len(keyword)):
-		if string == keyword[i]:
+		if token == keyword[i]:
 			print("KEYWORD")
+			checkNotIdentifier = 1
 
 	for i in range(0, len(symbol1)):
-		if string == symbol1[i]:
+		if token == symbol1[i]:
 			print("ONE-CHAR OPERATOR")
 
 	for i in range(0, len(symbol2)):
-		if string == symbol2[i]:
+		if token == symbol2[i]:
 			print("TWO-CHAR OPERATOR")
 
-	for i in range(0, len(identifier)):
-		if string == identifier[i]:
-			print("IDENTIFIER")
+	for i in range(0, len(datatype)):
+		if token == datatype[i]:
+			print("DATA TYPE")
+			checkNotIdentifier = 1
+
+	for i in range(0, len(token)):
+		for j in range(0, len(identifiersHead)):
+			if (token[i] == identifiersHead[j]):
+				for m in range(i, len(token)):
+					for n in range(0, len(identifiersTail)):
+						if (token[m] == identifiersTail[n]):
+							checkIdentifier = 1
+				break;
+
+	if (checkIdentifier == 1 and checkNotIdentifier == 0):
+		print("IDENTIFIER")
+
+
 
 
 def scanner():
@@ -41,16 +80,16 @@ def scanner():
 	nextChar = f.read(1)
 	return nextChar
 
-string = ""
+token = ""
 while f.read(1):
 	f.seek(-1,1)
 	c = scanner()
 	# print("CHARACTER: " + c)
 	if (c != ' ' and c != '\n'):
-		string = string + c
+		token = token + c
 		# print(string)
 	else:
-		lexer(string)
-		string = ""
+		lexer(token)
+		token = ""
 
 f.close()
